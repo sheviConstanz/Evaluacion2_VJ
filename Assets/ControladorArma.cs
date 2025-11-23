320043c3438573b8e65f48d272cd33ba099b5938
Rsanchezm <capitalcatto@gmail.com>
2025-11-23 14:58:59 -0300

    arma ajustada al personaje
    arma ajustada a el personaje

--- Assets/ControladorArma.cs
+++ Assets/ControladorArma.cs
@@ -1,19 +1,54 @@
 using UnityEngine;
-using UnityEngine.Events;
 
-public class ControladorArma : MonoBehaviour
+public class ControladorEspada : MonoBehaviour
 {
-    public ControladorObjetivoGolpeable objetivoGolpeado;
-    public float ataque = 5f;
+    [Header("Asigna aquí tus objetos desde la Jerarquía")]
+    public GameObject objetoEspada;     // Arrastra aquí tu objeto "Espada"
+    public Transform anclaCinto;        // Arrastra aquí tu "AnclaArmaCinto"
+    public Transform anclaMano;         // Arrastra aquí tu "Ancla"
 
+    // Variable interna para saber si la tienes en la mano o no
+    private bool estaEquipada = false;
 
-    void OnTriggerEnter(Collider other)
+    void Update()
     {
-        if(other.gameObject.tag == "Golpeable")
+        // Detectar si presionas la tecla 'F'
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            Debug.Log("GOLPEE ALGO");
-            objetivoGolpeado = other.gameObject.GetComponent<ControladorObjetivoGolpeable>();
-            objetivoGolpeado.alGolpearObjetivo.Invoke(ataque);
+            if (estaEquipada)
+            {
+                Guardar();
+            }
+            else
+            {
+                Sacar();
+            }
         }
+    }
+
+    void Sacar()
+    {
+        // 1. Cambiamos el padre a la mano (Ancla)
+        objetoEspada.transform.SetParent(anclaMano);
+
+        // 2. Reseteamos posición y rotación para que se ajuste exacto al grip
+        objetoEspada.transform.localPosition = Vector3.zero;
+        objetoEspada.transform.localRotation = Quaternion.identity;
+
+        estaEquipada = true;
+        Debug.Log("Espada equipada en mano");
+    }
+
+    void Guardar()
+    {
+        // 1. Cambiamos el padre al cinto (AnclaArmaCinto)
+        objetoEspada.transform.SetParent(anclaCinto);
+
+        // 2. Reseteamos posición y rotación
+        objetoEspada.transform.localPosition = Vector3.zero;
+        objetoEspada.transform.localRotation = Quaternion.identity;
+
+        estaEquipada = false;
+        Debug.Log("Espada guardada en el cinto");
     }
 }
